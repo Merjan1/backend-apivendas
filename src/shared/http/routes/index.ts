bb71ea04dbb236e1f9469f20efa39338726fdbe7
Merjan1 <jvictor669@hotmail.com>
2022-07-20 12:19:36 -0300

    orders - working

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
+
 import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
 import customersRouter from '@modules/customers/routes/customers.routes';
+import ordersRouter from '@modules/orders/routes/orders.routes';
 
 const routes = Router();
 
@@ -14,5 +16,6 @@ routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
 routes.use('/customers', customersRouter);
+routes.use('/orders', ordersRouter);
 
 export default routes;
