dac06a3ec20a18aa37fce1b09cc0fa36c27d30ac
Merjan1 <jvictor669@hotmail.com>
2022-07-20 12:27:40 -0300

    customer pagination

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,6 +3,8 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import { errors } from 'celebrate';
+import { pagination } from 'typeorm-pagination';
+
 import routes from './routes/index';
 import AppError from '../errors/AppError';
 import '@shared/typeorm';
@@ -12,6 +14,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(pagination);
 
 app.use('/files', express.static(uploadConfig.directory));
 
